9a34f279e4d56a27b8fd47a6c68819c49deebd3e
Szymon Roszyk <roszyk.szymon@gmail.com>
2024-10-26 17:17:20 +0200

    Added group list

--- src/group-card/GroupCard.tsx
+++ src/group-card/GroupCard.tsx
@@ -1,15 +1,38 @@
 import { Group } from "../models/group";
-import "./GroupCard.module.css";
+import styles from "./GroupCard.module.css";
 
 export const GroupCard = ({ group }: { group: Group }) => {
+  const dayMapping: { [key: string]: string } = {
+    pon: "poniedziałek",
+    wto: "wtorek",
+    sro: "środa",
+    czw: "czwartek",
+    pia: "piątek",
+    sob: "sobota",
+    nie: "niedziela",
+  };
+
+  const getFormattedGroupDays = (groupDays: string): string => {
+    return groupDays
+      .split("-")
+      .map((day) => dayMapping[day] || day)
+      .join("-");
+  };
+
+  const getFormattedGroupHours = (groupHours: string): string => {
+    return groupHours.split("$")[0];
+  };
+
   return (
-    <div>
-      <div>{group.groupType}</div>
-      <div>{group.groupLevel}</div>
-      <div>
-        {group.groupDays} - {group.groupHours}
+    <div className={styles.groupCard}>
+      <div className={styles.groupType}>{group.groupType}</div>
+      <div className={styles.groupLevel}>{group.groupLevel}</div>
+      <div className={styles.groupDays}>
+        {getFormattedGroupDays(group.groupDays)}{" "}
+        {getFormattedGroupHours(group.groupHours)}
       </div>
-      <div>{group.groupLector}</div>
+      <div className={styles.groupLector}>{group.groupLector}</div>
+      <div className={styles.showMoreBtn}>więcej info →</div>
     </div>
   );
 };
--- src/groups-list/GroupsList.tsx
+++ src/groups-list/GroupsList.tsx
@@ -1,11 +1,13 @@
 import { GroupCard } from "../group-card/GroupCard";
 import { Group } from "../models/group";
 
+import styles from "./GroupsList.module.css";
+
 export const GroupsList = ({ groups }: { groups: Group[] }) => {
   return (
-    <div>
+    <div className={styles.groupList}>
       {groups.map((group) => (
-        <GroupCard group={group} />
+        <GroupCard group={group} key={group.groupId} />
       ))}
     </div>
   );
