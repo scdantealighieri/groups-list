03755077d6cbcd325ed879f48b7fec7dd1a6b766
sroszyk <roszyk.szymon@gmail.com>
2026-04-29 20:47:31 +0200

    fixed bug with groupId wrong data type

--- src/utils/group-utils.ts
+++ src/utils/group-utils.ts
@@ -2,13 +2,13 @@ import { DuettoGroup } from "../special-groups/duetto-group";
 import { IndividualGroup } from "../special-groups/individual-group";
 
 export function isSpecialGroup(groupId: string): boolean {
-    return groupId.startsWith("special_");
+  return groupId.toString().startsWith("special_");
 }
 
 export function isIndividualGroup(groupId: string): boolean {
-    return groupId === IndividualGroup.groupId;
+  return groupId === IndividualGroup.groupId;
 }
 
 export function isDuettoGroup(groupId: string): boolean {
-    return groupId === DuettoGroup.groupId;
+  return groupId === DuettoGroup.groupId;
 }
